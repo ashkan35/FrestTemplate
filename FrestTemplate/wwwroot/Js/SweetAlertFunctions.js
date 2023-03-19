f0e7fdaffae76673ed314a357aa5e4a4705a5666
ashkan semsar <ashkansemsar@hotmail.com>
2023-03-19 17:49:00 +0330

    .

--- FrestTemplate/wwwroot/Js/SweetAlertFunctions.js
+++ FrestTemplate/wwwroot/Js/SweetAlertFunctions.js
@@ -1,3 +1,12 @@
-﻿(function () {
-    alert("ok");
-})();  
+﻿function SwalFire(text,title,icon,buttonClass,buttonText,buttonsStyling=false) {
+    Swal.fire({
+        title: 'خطا!',
+        text: ' شما روی دکمه کلیک کردید!',
+        icon: 'error',
+        customClass: {
+            confirmButton: 'btn btn-primary'
+        },
+        confirmButtonText: 'باشه',
+        buttonsStyling: false
+    });
+} 
