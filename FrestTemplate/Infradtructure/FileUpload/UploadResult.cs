90e11bc288480901f9f58dd3c45431ee34d70e01
ashkan semsar <ashkansemsar@hotmail.com>
2023-03-23 00:02:00 +0430

    .

---
+++ FrestTemplate/Infradtructure/FileUpload/UploadResult.cs
@@ -0,0 +1,21 @@
+﻿namespace FrestTemplate.Infradtructure.FileUpload;
+
+public class UploadResult
+{
+    public UploadResult()
+    {
+        Data = new();
+    }
+    public Data Data { get; set; }
+    public bool IsSuccess { get; set; } = false;
+    public int StatusCode { get; set; }
+    public string Message { get; set; } = "";
+    public string RequestId { get; set; } = "";   
+}
+
+public class Data
+{
+    public string FileGuid { get; set; } = "";
+    public string FileExtention { get; set; } = "";
+    public string FileName { get; set; } = "";
+}
--- FrestTemplate/Program.cs
+++ FrestTemplate/Program.cs
@@ -1,4 +1,5 @@
 using FrestTemplate;
+using FrestTemplate.Infradtructure;
 using FrestTemplate.Infradtructure.JsServices.SweetAlert;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -6,7 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<ISweetAlertService, SweetAlertService>();
+builder.Services.AddScoped(sp => new HttpClient ());
 await builder.Build().RunAsync();
