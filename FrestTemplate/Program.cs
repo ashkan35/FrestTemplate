8578729dc876c6fb74c64810eb171d3081fe822e
ashkan semsar <ashkansemsar@hotmail.com>
2023-03-25 18:30:42 +0330

    grid test

--- FrestTemplate/Program.cs
+++ FrestTemplate/Program.cs
@@ -3,6 +3,7 @@ using FrestTemplate.Infra.JsServices.SweetAlert;
 using FrestTemplate.Infra;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Havit.Blazor.Components.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,4 +11,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<ISweetAlertService, SweetAlertService>();
 builder.Services.AddScoped(sp => new HttpClient ());
+builder.Services.AddHxServices();
 await builder.Build().RunAsync();
