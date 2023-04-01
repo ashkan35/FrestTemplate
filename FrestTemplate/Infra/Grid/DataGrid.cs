c0eb5547e9d5c17922a9554d83a8226f4c39c434
ashkan semsar <ashkansemsar@hotmail.com>
2023-04-01 23:29:02 +0330

    data grid settings

---
+++ FrestTemplate/Infra/Grid/DataGrid.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+using Havit.Blazor.Components.Web.Bootstrap;
+using Microsoft.AspNetCore.Components;
+
+namespace FrestTemplate.Infra.Grid
+{
+    [CascadingTypeParameter(nameof(TItem))]
+
+    public class DataGrid<TItem> : HxGrid<TItem>
+    {
+        public DataGrid()
+        {
+            TableCssClass = "table-bordered";
+            PagerSettings = new PagerSettings
+            {
+                FirstPageIcon = BootstrapIcon.ArrowRightCircleFill,
+                PreviousPageIcon = BootstrapIcon.ArrowRightCircle,
+                NextPageIcon = BootstrapIcon.ArrowLeftCircle,
+                LastPageIcon = BootstrapIcon.ArrowLeftCircleFill,
+            };
+
+        }
+
+    }
+}
